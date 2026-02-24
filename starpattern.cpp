c7e58351cd0282e8bbc172dc5c4677b78530736d
Divyansh <166783788+Divysak@users.noreply.github.com>
2026-02-24 19:21:15 +0530

    Add files via upload

--- starpattern.cpp
+++ starpattern.cpp
@@ -5,28 +5,33 @@
 */
 #include <iostream>
 using namespace std;
-int main()
-{
-int n, i, j;
-cout<<"enter the number of rows:"<<endl;
-cin>>n;
-for(i=1 ; i<=n ; i++)
-{
-    for(j=1 ; j<=i ; j++)
-    {
-        cout<<"* ";
-    }
-    for(j=1 ; j<=2*(n-i) ; j++)
-    {
-        cout<<"  ";
-    }
-    for(j=1 ; j<=i ; j++)
-    {
-        cout<<"* ";
-    }
-    cout<<endl;
 
-}
-return 0;
-}
+int main() 
+{
+    int n = 3;   // number of rows
 
+    for(int i = 1; i <= n; i++)
+    {
+        // Left stars
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        // Spaces in middle
+        for(int k = 1; k <= 2*(n - i); k++)
+        {
+            cout << "  ";
+        }
+
+        // Right stars
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
